fef900d99b71856f5c0788264e8d9ce23276bb01
Boyko Nikolaev <boykonikolaev1997@gmail.com>
2023-12-13 20:53:58 +0200

    SearchController with mongodb

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,3 +1,6 @@
+
+using SearchService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,10 +11,17 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
+
+try
+{
+	await DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+	Console.WriteLine(e);
+}
 
 app.Run();
