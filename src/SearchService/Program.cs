03c0e2f1cebbba9b820109488c5c91913c00e61a
Boyko Nikolaev <boykonikolaev1997@gmail.com>
2023-12-20 17:27:40 +0200

    Added MassTransit and contracts

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,5 +1,6 @@
 
 using System.Net;
+using MassTransit;
 using Polly;
 using Polly.Extensions.Http;
 using SearchService.Data;
@@ -12,6 +13,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddHttpClient<AuctionSvcHttpClient>()
 	.AddPolicyHandler(GetPolicy());
+
+builder.Services.AddMassTransit(x => 
+{
+	x.UsingRabbitMq((context, cfg) => 
+	{
+		cfg.ConfigureEndpoints(context);
+	});
+});
 
 var app = builder.Build();
 
