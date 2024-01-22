28285654a26cad8fa150d4a1f516c55a9e53b550
Boyko Nikolaev <boykonikolaev1997@gmail.com>
2024-01-22 22:34:24 +0200

    Updates to authentication in frontend

--- frontend/web-app/app/actions/authActions.ts
+++ frontend/web-app/app/actions/authActions.ts
@@ -1,11 +1,15 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
+import { getToken } from "next-auth/jwt";
+import { cookies, headers } from 'next/headers'
+import { NextApiRequest } from "next";
 
 export async function getSession() {
     return await getServerSession(authOptions);
 }
 
 export async function getCurrentUser() {
+
     try {
         const session = await getSession();
 
@@ -16,4 +20,17 @@ export async function getCurrentUser() {
     } catch (error) {
         return null;
     }
+}
+
+export async function getTokenWorkaround() {
+    const req = {
+        headers: Object.fromEntries(headers() as Headers),
+        cookies: Object.fromEntries(
+            cookies()
+                .getAll()
+                .map(c => [c.name, c.value])
+        )
+    } as NextApiRequest;
+
+    return await getToken({req});
 }
