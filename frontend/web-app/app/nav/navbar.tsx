05116a75f6d16ecfba4e63cd2a9ecfe65e287591
Boyko Nikolaev <boykonikolaev1997@gmail.com>
2024-01-22 21:58:06 +0200

    User menu in frontend and logoud

--- frontend/web-app/app/nav/navbar.tsx
+++ frontend/web-app/app/nav/navbar.tsx
@@ -2,13 +2,21 @@ import React from 'react'
 import Search from './Search'
 import Logo from './Logo'
 import LoginButton from './LoginButton'
+import { getCurrentUser } from '../actions/authActions'
+import UserActions from './userActions'
 
-export default function Navbar() {
+export default async function Navbar() {
+  const user = await getCurrentUser();
+
   return (
     <header className='sticky top-0 z-50 flex justify-between bg-white p-5 items-center text-gray-800 shadow-md'>
       <Logo />
       <Search />
-      <LoginButton />
+      {user ? (
+        <UserActions user={user}/>
+      ) : (
+        <LoginButton />
+      )}
     </header>
   )
 }
