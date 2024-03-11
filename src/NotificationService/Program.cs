7fe4a1a41837f4a2150f2e026a60bf27c01f2198
Boyko Nikolaev <boykonikolaev1997@gmail.com>
2024-03-11 11:01:47 +0200

    SignalR in NotificationService

---
+++ src/NotificationService/Program.cs
@@ -0,0 +1,29 @@
+using MassTransit;
+using NotificationService.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMassTransit(x => 
+{			
+	x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("nt", false));
+	
+	x.UsingRabbitMq((context, cfg) => 
+	{
+		cfg.Host(builder.Configuration["RabbitMq:Host"], "/", host => 
+		{
+			// guest is the default value in case if nothing is found in the configuration
+			host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+			host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+		});
+		
+		cfg.ConfigureEndpoints(context);
+	});
+});
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+app.MapHub<NotificationHub>("/notifications");
+
+app.Run();
