512c170c0fa0b2624b127d3cb304704b8e0714ec
Boyko Nikolaev <boykonikolaev1997@gmail.com>
2024-03-12 21:47:41 +0200

    signalR in front end

--- frontend/web-app/app/auctions/CountdownTimer.tsx
+++ frontend/web-app/app/auctions/CountdownTimer.tsx
@@ -1,7 +1,10 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 'use client'
 
 import React from 'react'
 import Countdown, { zeroPad } from 'react-countdown';
+import { useBidStore } from '../hooks/useBidStore';
+import { usePathname } from 'next/navigation';
 
 type Props = {
     auctionEnd: string
@@ -28,9 +31,18 @@ const renderer = ({ days, hours, minutes, seconds, completed }:
   };
 
 export default function CountdownTimer({auctionEnd}: Props) {
+    const setOpen = useBidStore(state => state.setOpen);
+    const pathname = usePathname();
+
+    function auctionFinished() {
+        if (pathname.startsWith('/auctions/details')) {
+            setOpen(false);
+        }
+    }
+
   return (
     <div>
-        <Countdown date={auctionEnd} renderer={renderer} />
+        <Countdown date={auctionEnd} renderer={renderer} onComplete={auctionFinished}/>
     </div>
   )
 }
--- frontend/web-app/app/auctions/CurrentBid.tsx
+++ frontend/web-app/app/auctions/CurrentBid.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { numberWithCommas } from '../lib/numberWithComa';
 
 type Props = {
     amount?: number
@@ -6,7 +7,7 @@ type Props = {
 }
 
 export default function CurrentBid({ amount, reservePrice }: Props) {
-    const text = amount ? '$' + amount : 'No bids';
+    const text = amount ? '$' + numberWithCommas(amount) : 'No bids';
     const color = amount ? amount > reservePrice ? 'bg-green-600' : 'bg-amber-600' : 'bg-red-600';
 
     return (
